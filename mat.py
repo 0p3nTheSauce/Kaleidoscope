d86df5119053ceccfaf32d32b26eacfb4c8e142d
Luke Goodall <ljgoodall2001@gmail.com>
2024-10-24 17:27:17 +0200

    added some mirror lines'

---
+++ mat.py
@@ -0,0 +1,78 @@
+import numpy as np
+import sys
+
+
+
+def mir(A):
+  C = A.T
+  D = np.flip(C, 0)
+  E = np.flip(D, 1)
+  return E
+
+def mir2(A):
+  # C = A.T
+  D = np.flip(A, 0)
+  E = np.flip(D, 1)
+  return E
+
+def mir_diag(m, d):
+  if d == 1:
+    return mir(m)
+  elif d == -1:
+    return mir2(m)
+  else:
+    #error
+    print("invalid diagonal")
+    sys.exit(1)
+
+
+
+if __name__ == '__main__':
+  # Define two matrices
+  A = np.array([[1, 2, 0],
+                [4, 0, 0],
+                [0, 0, 0]])
+
+  B = np.array([[0, 0, 0],
+                [0, 0, 2],
+                [0, 4, 1]])
+
+  # print("Matrix A\n", A)
+
+  # C = A.T
+  # print("transpose of A\n", C)
+
+  # D = np.flip(A, 0)
+  # print("0(vertical) flip of A\n", D)
+
+  # E = np.flip(A, 1)
+  # print("1(horizontal) flip of A\n", E)
+
+  # print("####################")
+
+  print("Matrix A\n", A)
+  C = A.T
+  print("transpose of A\n", C)
+  D = np.flip(C, 0)
+  print("0(vertical) flip\n", D)
+  E = np.flip(D, 1)
+  print("1(horizontal) flip\n", E)
+
+
+
+
+  print("B\n", B)
+    
+  print("mir(B)\n", mir(B))
+
+  Z = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+  print("Z\n", Z)
+  print("mir(Z)\n", mir(Z))
+
+
+
+    
+  print("mir2(Z)\n", mir2(Z))
--- mirror.py
+++ mirror.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
 import cv2
 import numpy as np
+import sys
+#local
+from mat import mir_diag
 
-def mirror(img, line):
-  m, c = line
-  h, w, _ = img.shape
-  cp = img.copy()
-  for y in range(h):
-    for x in range(w):
-      yl = m * x + c
-      xl = round((y - c) / m)
-      if y > yl:
-        cp[y, x] = img[yl, xl]
-  return cp
+# def mirror(img, line):
+#   m, c = line
+#   h, w, _ = img.shape
+#   cp = img.copy()
+#   for y in range(h):
+#     for x in range(w):
+#       yl = m * x + c
+#       xl = round((y - c) / m)
+#       if y > yl:
+#         cp[y, x] = img[yl, xl]
+#   return cp
 
-def mirror2(img, line):
-  m, c = line
-  sqr = crop_square(img)
-  h, w, _ = sqr.shape
-  cp = sqr.copy()
-  c = h - c
-  m = -m
-  for y in range(h):
-    for x in range(w):
-      yl = m * x + c
-      if y == yl:
-        cp[y, x] = (0, 0, 255)
-      elif y < yl:
-        cp[x, y] = sqr[y, x]
+# def mirror2(img, line):
+#   m, c = line
+#   sqr = crop_square(img)
+#   h, w, _ = sqr.shape
+#   cp = sqr.copy()
+#   c = h - c
+#   m = -m
+#   for y in range(h):
+#     for x in range(w):
+#       yl = m * x + c
+#       if y == yl:
+#         cp[y, x] = (0, 0, 255)
+#       elif y < yl:
+#         cp[x, y] = sqr[y, x]
   
-  return cp
+#   return cp
 
 
 
@@ -39,58 +42,106 @@ def crop_square(img):
     return img[:w, :]
   return img[:, :h]
 
-def mirror3(img, line):
-  m, c = line
-  sqr = crop_square(img)
-  h, w, _ = sqr.shape
-  cp = sqr.copy()
-  for y in range(h):
-    for x in range(w):
-      yl = m * x + c
-      xl = round((y - c) / m)
-      if y > yl:
-        cp[y, x] = (0, 0, 0)
-  flip = cv2.flip(cp, 1)
-  # cv2.imshow('Flip', flip)
-  # cv2.waitKey(0)
-  M = cv2.getRotationMatrix2D((w // 2, h // 2), 90, 1)
-  rot = cv2.warpAffine(flip, M, (w, h))
-  # cv2.imshow('Rotated', rot)
-  # cv2.waitKey(0)
+# def mirror3(img, line):
+#   m, c = line
+#   sqr = crop_square(img)
+#   h, w, _ = sqr.shape
+#   cp = sqr.copy()
+#   for y in range(h):
+#     for x in range(w):
+#       yl = m * x + c
+#       xl = round((y - c) / m)
+#       if y > yl:
+#         cp[y, x] = (0, 0, 0)
+#   flip = cv2.flip(cp, 1)
+#   # cv2.imshow('Flip', flip)
+#   # cv2.waitKey(0)
+#   M = cv2.getRotationMatrix2D((w // 2, h // 2), 90, 1)
+#   rot = cv2.warpAffine(flip, M, (w, h))
+#   # cv2.imshow('Rotated', rot)
+#   # cv2.waitKey(0)
   
-  merged = cv2.addWeighted(cp, 1.0, rot, 1.0, 0)
+#   merged = cv2.addWeighted(cp, 1.0, rot, 1.0, 0)
   
-  return merged
+#   return merged
 
-def mirror4(img, line):
-  m, c = line
-  sqr = crop_square(img)
-  h, w, _ = sqr.shape
-  cp = sqr.copy()
+# def mirror4(img, line):
+#   m, c = line
+#   sqr = crop_square(img)
+#   h, w, _ = sqr.shape
+#   cp = sqr.copy()
+#   for y in range(h):
+#     for x in range(w):
+#       yl = m * x + c
+#       if y < yl:
+#         cp[x, y] = sqr[y, x]
+  
+#   return cp
+
+
+def mirror(img, line):
+  if line == 'v':
+    return cv2.flip(img, 0)
+  elif line == 'h':
+    return cv2.flip(img, 1)
+  elif line == 'p': #positive incline diagonal
+    return mir_diag(img, 1)
+  elif line == 'n':
+    return mir_diag(img, -1) #negative incline diagonal
+  else:
+    print("Invalid line")
+    sys.exit(1)
+
+def blackout(img, line):
+  # sqr = crop_square(img)
+  h, w, _ = img.shape
+  bl = img.copy()
+  if line == 'v':
+    bl[:h // 2, :, :] = (0, 0, 0)
+    return bl
+  elif line == 'h':
+    bl[:, :w // 2, :] = (0, 0, 0)
+    return bl
+  elif line == 'p':  
+    m, c = 1, 0
+  elif line == 'n':
+    m, c = -1, h
+  else:
+    print("Invalid line")
+    sys.exit(1)
+  c = h - c
+  m = -m
   for y in range(h):
     for x in range(w):
       yl = m * x + c
+      # if y == yl:
+      #   bl[y, x] = (0, 0, 255)
       if y < yl:
-        cp[x, y] = sqr[y, x]
+        bl[x, y] = (0, 0, 0)
+  return bl
+
+def half_mirror(img, line):
+  sqr = crop_square(img)
+  cv2.imshow('Square', sqr)
+  cv2.waitKey(0)
+  bl = blackout(sqr, line)
+  cv2.imshow('Blackout', bl)
+  cv2.waitKey(0)
+  mr = mirror(bl, line)
+  cv2.imshow('Mirror', mr)
+  cv2.waitKey(0)
+  return cv2.addWeighted(bl, 1.0, mr, 1.0, 0)
+
   
-  return cp
-
-
-    
 
 def main():
   img = cv2.imread('me.jpg')
   cv2.imshow('Original', img)
   cv2.waitKey(0)
-  sqr = crop_square(img)
-  cv2.imshow('Square', sqr)
-  cv2.waitKey(0)  
-  print(sqr.shape)
-  mir = mirror4(img, (0, 240))
-  cv2.imshow('Mirror', mir)
+  
+  hm = half_mirror(img, 'v')
+  cv2.imshow('Half Mirror', hm)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
-  pass
-
 if __name__ == '__main__':
   main()
