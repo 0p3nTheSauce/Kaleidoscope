7bd5a53aa082bd3774922b5bb6b9ed71e0ea7f1c
Luke Goodall <ljgoodall2001@gmail.com>
2025-10-15 23:29:33 +0200

    seperated unfinished features into dev branch

--- merge_mp3_4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $# -lt 4 ]; then
-  ffmpeg -i "$1" -i "$2" -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 "$3"
-else
-  echo "Usage: $0 <input_video.mp4> <input_audio.mp3> <output_with_audio.mp4>"
-fi
